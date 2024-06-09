1236a29b67d10a9d48f1afe2f4d63334e9333c7f
Ritesh Singh <riteshsingh3628@gmail.com>
2024-06-09 20:01:34 +0530

    add:initial files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './components/home'
+import Card from './components/Card'
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Card />
+    </div>
+  )
+}
+
+export default App
